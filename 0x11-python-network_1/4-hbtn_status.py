4ba862b74ed45323abf622096455744ad76ca04b
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-03-28 13:01:20 +0300

    I made some changes to 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+
+"""
+This script fetches the status from https://alx-intranet.hbtn.io/status using the requests package.
+It then displays the body of the response in a specific format.
+"""
+
 import requests
 
+def fetch_status():
+    """
+    Fetches the status from https://alx-intranet.hbtn.io/status.
+    """
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    return response.text
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    status_body = fetch_status()
+
+    # Displaying the body of the response
+    print("- Body response:")
+    print("\t- type:", type(status_body))
+    print("\t- content:", status_body)
