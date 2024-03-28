4a7712348a177f3f0bb5ef5211593e880e89802a
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-03-28 13:08:35 +0300

    I made some changes to 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-""" Uses requests module -- yay"""
+"""Fetches the URL: https://intranet.hbtn.io/status
+with `requests` module
+"""
+
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {_type}'.format(_type=type(req.text)))
+    print('\t- content: {_content}'.format(_content=req.text))
